ab67d48a13cb7331869aed0b3a7b41fb8a76c74b
sydneycode <sstaggs12@gmail.com>
2018-10-23 10:48:53 -0700

    adding initial files

---
+++ models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Book: require("./book"),
+  User: require("./user"),
+  FoodEntry: require("/.foodEntry"),
+  WaterEntry: require("/.waterEntry"),
+  WeightEntry: require("/weightEntry")
+
+};
